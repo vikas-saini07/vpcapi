fa9f8bbff166878329737f701f0391113551a55f
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-01-28 18:10:44 +1000

    Update simplesessionNEW.php

--- simplesessionNEW.php
+++ simplesessionNEW.php
@@ -1,8 +1,8 @@
 <?php
 include '_bootstrap.php';
 $curl = curl_init();
-//$username = "merchant.TESTVICTESNB237";
-//$password = "ed17a6e2ed43172c0f7bde912cbe54de";
+$username = "merchant.TESTVICTESNB237";
+$password = "ed17a6e2ed43172c0f7bde912cbe54de";
 $newdata = array("lineOfBusiness" => "test_socks");
 $postdata = json_encode($newdata);
 curl_setopt_array($curl, array(
@@ -13,11 +13,12 @@ curl_setopt_array($curl, array(
   CURLOPT_TIMEOUT => 400,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
-  //CURLOPT_POSTFIELDS => "$postdata",
-  CURLOPT_HTTPHEADER => array(
+  CURLOPT_POSTFIELDS => "$postdata",
+  CURLOPT_USERPWD => "$username:$password"
+  /*CURLOPT_HTTPHEADER => array(
     'Content-Type: application/json',
     'Authorization: Basic ' . base64_encode("$username:$password")
-));
+)*/);
 $response = curl_exec($curl);
 $err = curl_error($curl);
 curl_close($curl);
