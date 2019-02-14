1a31fdf6658600dd7ca6e787781dcb8770520f28
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-02-14 18:27:56 +1000

    Update SecureHash.php

--- SecureHash.php
+++ SecureHash.php
@@ -1,7 +1,5 @@
 <?php
 global $securesecretvar;  
  $securesecretvar = $_POST["vpc_SecureHash"];
-include('PHP_VPC_3DS 2.5 Party_DO.php');
-include('PHP_VPC_3DS 2.5 Party_DR.php');
 echo $securesecretvar;
 ?>
