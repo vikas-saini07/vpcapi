8bc4dad3258ebbb16a043379f126d3b7f65b0454
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-02-14 17:14:59 +1000

    Update PHP_VPC_3DS 2.5 Party_DR.php

--- PHP_VPC_3DS 2.5 Party_DR.php
+++ PHP_VPC_3DS 2.5 Party_DR.php
@@ -12,7 +12,8 @@ $conn = new VPCPaymentConnection();
 // This is secret for encoding the SHA256 hash
 // This secret will vary from merchant to merchant
 
-$secureSecret = "6FE538069493E5109884E726715FAEDC";
+$secureSecret = $_POST["vpc_SecureHash"];
+	//"6FE538069493E5109884E726715FAEDC";
 	//$_GET["vpc_SecureHash"]; 
 //"512ACA46E9A1F9C55013221B2220B1D9";
 
