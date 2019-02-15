ea3bb9c943ab033f85dfd6644f057cb66c8980eb
victor0888 <47068746+victor0888@users.noreply.github.com>
2019-02-15 13:06:31 +1000

    Update PHP_VPC_3DS 2.5 Party_DO.php

--- PHP_VPC_3DS 2.5 Party_DO.php
+++ PHP_VPC_3DS 2.5 Party_DO.php
@@ -27,7 +27,7 @@ $conn = new VPCPaymentConnection();
 
 
 // Set the Secure Hash Secret used by the VPC connection object
-$conn->setSecureSecret($securesecret);
+$conn->setSecureSecret($securesecretvar);
 
 
 // *******************************************
@@ -84,7 +84,7 @@ $appendAmp = 0;
 $hashinput = rtrim($hashinput, "&");
 ?>		
 	<!-- attach SecureHash -->
-    <input type="hidden" name="vpc_SecureHash" value="<?php echo(strtoupper(hash_hmac('SHA256', $hashinput, pack('H*',$securesecret)))); ?>"/>
+    <input type="hidden" name="vpc_SecureHash" value="<?php echo(strtoupper(hash_hmac('SHA256', $hashinput, pack('H*',$securesecretvar)))); ?>"/>
 		<input type="hidden" name="vpc_SecureHashType" value="SHA256">
 </td></tr>
 </table>
